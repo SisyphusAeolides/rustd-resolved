c60d5ce2b102bf0ccd5240aa65a3a580021c8083
Kenny Glowner <SisyphusAeolides@pm.me>
2026-08-17 10:50:18 -0500

    Canonicalize resolver source formatting

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
     clippy::missing_errors_doc,
     clippy::missing_panics_doc,
     clippy::module_name_repetitions,
-    clippy::must_use_candidate,
+    clippy::must_use_candidate
 )]
 
 extern crate self as rustd_resolved;
